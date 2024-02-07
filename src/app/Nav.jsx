2244832784562a3c2b0195af8c8651a7ba18d23b
Indiana Meregone <juanmeregone@gmail.com>
2024-02-07 14:28:35 -0300

    dando un poco de estilo a la pagina!

--- src/app/Nav.jsx
+++ src/app/Nav.jsx
@@ -1,20 +1,22 @@
 import Link from 'next/link';
 import './page.module.css'
+import { Button } from '@mui/material';
 export const Nav = ()=>{
     return(
         <nav>
             <ul style={{display:"flex", height:"50px",width: "100%", backgroundColor: "blue", alignItems:"center",justifyContent:"center",gap:"10px"}}>
                 <li style={{listStyle:"none"}}>
-                <Link href={'/'} style={{color:"#ffffff"}}>Inicio</Link>
+                <Button variant="contained"><Link href={'/home'} style={{color:"#ffffff",textDecoration:"none"}}>Inicio</Link></Button>               
                 </li>
                 <li style={{listStyle:"none"}}>
-                <Link href={'/empresa'} style={{color:"#ffffff"}}>Empresa</Link>
+                <Button variant="contained"><Link href={'/empresa'} style={{color:"#ffffff",textDecoration:"none"}}>Empresa</Link></Button>                 
                 </li>
                 <li style={{listStyle:"none"}}>
-                <Link href={'/proyecto'} style={{color:"#ffffff"}}>Proyecto</Link>
+                <Button variant="contained"><Link href={'/proyecto'} style={{color:"#ffffff",textDecoration:"none"}}>Proyecto</Link></Button>
                 </li>
                 <li style={{listStyle:"none"}}>
-                <Link href={'/contacto'} style={{color:"#ffffff"}}>Contacto</Link>
+                <Button variant="contained"><Link href={'/contacto'} style={{color:"#ffffff",textDecoration:"none"}}>Contacto</Link></Button>
+                
                 </li>
             </ul>
         </nav>
--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,6 +1,11 @@
+import { Nav } from "../Nav"
+
 const Home =()=>{
     return(
+        <>
+        <Nav/>
         <h2>Home</h2>
+        </>
     )
 }
 export default Home
