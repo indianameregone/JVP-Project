5703cdbce251da766c881d41690d9bc9576e5059
Martin Tomeo <martintomeo19@gmail.com>
2024-02-07 13:04:49 -0500

    carpeta de pagina proyectos

---
+++ src/app/proyectos/ListaBotones.jsx
@@ -0,0 +1,26 @@
+import './style.css'
+
+
+export function ListaBotones(name, size){
+    const titulo = name;
+    const largo = size;
+    const botones = Array.from({ length: largo }, (_, index) => (
+        <div key={index}>
+             <button>Boton {index + 1}</button>
+             </div>
+      ));
+
+    return(
+    <>
+        <div>
+            <h2>{titulo}</h2>
+        </div>
+        <div>
+            {botones}
+        </div>
+    </>
+
+       
+    )
+
+}
---
+++ src/app/proyectos/page.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+import { ListaBotones } from './ListaBotones'
+
+export function Proyectos(){
+
+    return(
+        <>
+            <ListaBotones name="Test" size="5"/>
+        </>
+    )
+
+}
