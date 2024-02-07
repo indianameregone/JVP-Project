93a32b531551d8988df4e7c1e024dfa30bff34bc
Indiana Meregone <juanmeregone@gmail.com>
2024-02-07 12:38:08 -0300

    cambio en nav

--- src/app/Nav.jsx
+++ src/app/Nav.jsx
@@ -1,18 +1,20 @@
+import Link from 'next/link';
+import './page.module.css'
 export const Nav = ()=>{
     return(
         <nav>
-            <ul>
-                <li>
-                    Inicio
+            <ul style={{display:"flex", height:"50px",width: "100%", backgroundColor: "blue", alignItems:"center",justifyContent:"center",gap:"10px"}}>
+                <li style={{listStyle:"none"}}>
+                <Link href={'/'} style={{color:"#ffffff"}}>Inicio</Link>
                 </li>
-                <li>
-                    Empresa
+                <li style={{listStyle:"none"}}>
+                <Link href={'/empresa'} style={{color:"#ffffff"}}>Empresa</Link>
                 </li>
-                <li>
-                    Proyecto
+                <li style={{listStyle:"none"}}>
+                <Link href={'/proyecto'} style={{color:"#ffffff"}}>Proyecto</Link>
                 </li>
-                <li>
-                    Contacto
+                <li style={{listStyle:"none"}}>
+                <Link href={'/contacto'} style={{color:"#ffffff"}}>Contacto</Link>
                 </li>
             </ul>
         </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
-import styles from "./page.module.css";
+import './page.module.css'
 import { Nav } from "./Nav";
 
 export default function Home() {
   return (
-    <>
+    <div className='main-page'>
     <Nav/>
       <h1>JVP Consultores</h1>
-    </>
+    </div>
   );
 }
