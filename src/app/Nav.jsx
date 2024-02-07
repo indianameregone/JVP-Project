9a0a90b35bd286aa2f6d2483cfbd21165d34846a
Indiana Meregone <juanmeregone@gmail.com>
2024-02-07 11:56:02 -0300

    van los primeros cambios

---
+++ src/app/Nav.jsx
@@ -0,0 +1,20 @@
+export const Nav = ()=>{
+    return(
+        <nav>
+            <ul>
+                <li>
+                    Inicio
+                </li>
+                <li>
+                    Empresa
+                </li>
+                <li>
+                    Proyecto
+                </li>
+                <li>
+                    Contacto
+                </li>
+            </ul>
+        </nav>
+    )
+}
