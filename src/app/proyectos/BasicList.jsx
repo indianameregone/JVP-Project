da56ef4b89b060f98a78c63fdb504e128d2c530f
MartinTomeo <martintomeo19@gmail.com>
2024-02-09 13:33:40 -0500

    listas sin estilo en proyectos

--- src/app/proyectos/BasicList.jsx
+++ src/app/proyectos/BasicList.jsx
@@ -3,11 +3,9 @@ import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+
 
 
 
@@ -22,9 +20,6 @@ function BasicList({listItems}) {
           {listItems.map((item, index) => (
             <ListItem key={index} disablePadding>
               <ListItemButton>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <DraftsIcon />}
-                </ListItemIcon>
                 <ListItemText primary={item.text} />
               </ListItemButton>
             </ListItem>
