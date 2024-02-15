ff79e2de1007de1f5881d1e4e02a8d34f9758391
Indiana Meregone <juanmeregone@gmail.com>
2024-02-15 09:39:13 -0300

    cambios del dia

--- src/app/servicios/diseño.jsx
+++ src/app/servicios/diseño.jsx
@@ -1,5 +1,8 @@
 export const Diseño = () =>{
     return(
-        <h1>Diseño</h1>
+        <div style={{ width:"90%",height:"400px", marginTop:"20px"}}>
+            <h1>Diseño</h1>
+            <hr></hr>
+        </div>
     )
 }
--- src/app/servicios/main.jsx
+++ src/app/servicios/main.jsx
@@ -1,23 +1,25 @@
 import { Chip } from "@mui/material"
+import './style.css'
 export const Main = () =>{
     return(
         <div style={{ width:"90%",height:"400px", marginTop:"20px"}}>
             <h2>Servicios</h2>
-            <hr></hr>
             <p >En JVP Consultores acompañamos a nuestros clientes en todas las fases de los Proyectos y Operaciones. En cada Servicio que brindamos, además de considerar los impactos ambientales y asegurar la calidad e integridad física de todos los interesados, nos apoyamos en nuestro departamento de “Analítica” donde se desarrollan herramientas tecnológicas y digitales que permiten hacer un uso inteligente de los recursos</p>
-            <Chip style={{position: "absolute",top:"90%",left:"25%"}} label="Diseño de Ingenieria" />
-            <Chip style={{position: "absolute",top:"70%",left:"35%"}} label="Supervision de Obra" />
-            <Chip style={{position: "absolute",top:"50%",left:"45%"}} label="Quality Assurance" />
-            <Chip style={{position: "absolute",top:"50%",left:"60%"}} label="Asesoramiento" />
-            <Chip style={{position: "absolute",top:"70%",left:"70%"}} label="Auditoria" />
-            <Chip style={{position: "absolute",top:"90%",left:"85%"}} label="Consultoria" />
-            <div style={{width:"350px",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",position:"absolute",left:"45%",top:"80%"}}>
+            <button className="style-btn" style={{position: "absolute",top:"60%",left:"30%"}} >Diseño de Ingenieria</button> 
+            <button className="style-btn" style={{position: "absolute",top:"45%",left:"35%"}} >Supervision de Obra</button> 
+            <button className="style-btn" style={{position: "absolute",top:"35%",left:"45%"}} >Quality Assurance</button> 
+            <button className="style-btn" style={{position: "absolute",top:"35%",left:"60%"}} >Asesoramiento</button> 
+            <button className="style-btn" style={{position: "absolute",top:"45%",left:"70%"}} >Auditoria</button> 
+            <button className="style-btn" style={{position: "absolute",top:"60%",left:"75%"}} >Consultoria</button> 
+            <div style={{width:"350px",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column",position:"absolute",left:"45%",top:"55%"}}>
                 <p>Eficiencia Constructiva y Operativa</p>
                 <p>Información confiable</p> 
                 <p>Cumplimiento Regulatorio</p>
                 <p>Procedimientos Participativos </p>
+            </div> 
+            <div style={{backgroundColor:"#0E3354",borderRadius:"5px",padding:"4px",width:"75%",color:"white", display:"flex",justifyContent:"center",marginLeft:"15%",marginTop:"25%"}}>
+            <p>Experiencia</p>         
             </div>
-            
         </div>
     )
 }
