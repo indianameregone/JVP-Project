8abbc970edac2b2cc56b1a234c5ed206f3507981
Gonzalo Jauregui <gjauregui@jvpconsultores.com.ar>
2024-02-16 10:32:15 -0300

    otros

--- src/app/empresa/RSE.jsx
+++ src/app/empresa/RSE.jsx
@@ -1,10 +1,33 @@
-export const Rse = () =>{
-    return(
-        <div style={{width:'100%',marginTop:"10px",height:"80%"}}>
+import './style.css'
+import LHH from '../img/La huella hidrica.jpg'
+import LEC from '../img/La economia circular.jpg'
+import Image from "next/image"
+export const Rse = () => {
+    return (
+        <div className="general">
             <h2 >Responsabilidad Social Empresaria</h2>
             <hr></hr>
-            
-        
+            <br />
+            <p>En JVP Consultores entendemos que el agua dulce fue, es y será siempre un factor determinante en el
+                desarrollo económico y social de la humanidad, posee un rol básico y fundamental para asegurar y
+                mantener la integridad del entorno natural.</p>
+            <br />
+
+            <p>Al ser el agua un recurso natural vital, resulta entonces imperativo que los temas hídricos no sean abordados de
+                forma aislada. Debemos pensar y planificar los usos del agua para que esos usos sean eficaces, eficientes y
+                sostenibles en el tiempo. Estas premisas nos ha llevado como firma a ser especialistas en el manejo y gestión del
+                Ciclo Integral del Agua.
+            </p>
+            <br />
+            <p>En JVP Consultores hemos incorporado dos conceptos fundamentales en nuestra actividad como desarrolladores de proyectos
+                de agua y saneamiento:</p>
+            <Image src={LHH}/>
+            <Image src={LEC}/>
+        <p>Como se pronostica que la demanda mundial de agua excederá a los recursos viables disponibles, nuestros 
+            enfoques de diseño y operación, tanto para el agua potable como para el saneamiento y manejo de aguas 
+            pluviales, han dejado de ser los tradicionales, ya que en JVP Consultores hemos incorporado en nuestros 
+            Estudios y Proyectos, nuevas estrategias y tecnologías que posibilitan que este recurso vital sea usado y 
+            reutilizado, con criterios de uso y reúso eficientes para manejo y gestión del Ciclo Integral del Agua.</p>
         </div>
     )
 }
--- src/app/empresa/TransDig.jsx
+++ src/app/empresa/TransDig.jsx
@@ -1,10 +1,27 @@
-export const Transfdig = () =>{
-    return(
-        <div style={{width:'100%',marginTop:"10px",height:"80%"}}>
+export const Transfdig = () => {
+    return (
+        <div style={{ width: '100%', marginTop: "10px", height: "80%" }}>
             <h2 >Transformación Digital</h2>
             <hr></hr>
-            
-        
+            <br />
+            <p>Actualmente vivimos en un mundo donde los datos abundan y la complejidad radica en poder interpretarlos
+                , ellos son el input que se necesita para extraer información que posibilite mejorar la toma de decisiones.
+                En el mejor de los mundos será posible interpretarlos para poder anticiparse a futuros problemas o correr modelos
+                predictivos. La tendencia a la conectividad y uso de los dispositivos portátiles como herramienta de recolección
+                de información, ejecución de tareas o simple visualizadores son parte de la transformación digital a la que nadie
+                puede ni podrá escapar. Es por este motivo que JVP Consultores SA en el 2019 crea su departamento de Analítica
+                donde se desarrollan aplicaciones de base de datos y tableros de control con perspectivas hacia la Inteligencia
+                Artificial </p>
+
+            <br />
+            <p>Esta orientación hacia la Transformación permite entre muchas funcionalidades Agilizar procesos internos
+                 y documentarlos, además dentro de los Servicios Prestados permite el análisis de una gran cantidad de muestras
+                 (a veces hasta del Universo) obteniendo muy altos porcentajes en las estadísticas de probabilidades.
+                  El análisis de datos corre detrás de una serie de programaciones que se visualizan de forma sencilla, dinámica 
+                  y actualizada en tiempo real, brindando un gran Valor Agregado a sus Clientes.</p>
+            <br />
+            <img src="https://revistainnovacion.com/uploads/noticias/5/20210917175742_tr_digital_2.jpg" alt="Transformacion Digital" />
+
         </div>
     )
 }
